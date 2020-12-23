4b26f27ace841251410aeea46804cb4d5a51820d
Andi Dittrich <andi@andidittrich.de>
2020-12-23 18:59:30 +0100

    hypersolid v1.0.0 compatibility

--- scripts/post-multistrap.sh
+++ scripts/post-multistrap.sh
@@ -28,43 +28,46 @@ kernel_official(){
     esac
 
     # get kernel + precompiled modules/firmware
-    mkdir -p ${BUILDFS}/lib/modules/${RASPBERRY_KERNEL_VERSION}
+    mkdir -p ${TARGET_FS}/lib/modules/${RASPBERRY_KERNEL_VERSION}
     wget -O /tmp/raspberry-firmware.zip https://github.com/raspberrypi/firmware/archive/${RASPBERRY_FIRMWARE_RELEASE}.zip
     unzip -q /tmp/raspberry-firmware.zip -d /tmp
-    cp -R /tmp/firmware-${RASPBERRY_FIRMWARE_RELEASE}/boot/. ${BOOTFS}
-    cp -R /tmp/firmware-${RASPBERRY_FIRMWARE_RELEASE}/modules/${RASPBERRY_KERNEL_VERSION}/. ${BUILDFS}/lib/modules/${RASPBERRY_KERNEL_VERSION}
-    chmod -R 0777 ${BOOTFS}
+    cp -R /tmp/firmware-${RASPBERRY_FIRMWARE_RELEASE}/boot/. ${OUTPUT_DIR}
+    cp -R /tmp/firmware-${RASPBERRY_FIRMWARE_RELEASE}/modules/${RASPBERRY_KERNEL_VERSION}/. ${TARGET_FS}/lib/modules/${RASPBERRY_KERNEL_VERSION}
+    chmod -R 0777 ${OUTPUT_DIR}
 
     # download nonfree firmware (wifi..)
-    mkdir -p ${BUILDFS}/lib/firmware/brcm
-    cd ${BUILDFS}/lib/firmware/brcm
+    mkdir -p ${TARGET_FS}/lib/firmware/brcm
+    cd ${TARGET_FS}/lib/firmware/brcm
     wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.bin
     wget https://github.com/RPi-Distro/firmware-nonfree/raw/master/brcm/brcmfmac43430-sdio.txt
 
     # add kernel config to config.txt
-    echo "# -- hypersolid automated boot configuration" >> ${BOOTFS}/config.txt
-    echo "kernel=${RASPBERRY_KERNEL_NAME}" >> ${BOOTFS}/config.txt
+    echo "# -- hypersolid automated boot configuration" >> ${OUTPUT_DIR}/config.txt
+    echo "kernel=${RASPBERRY_KERNEL_NAME}" >> ${OUTPUT_DIR}/config.txt
 
     # 64bit kernel ?
     if [ "${RASPBERRY_KERNEL_NAME}" == "kernel8.img" ]; then
-        echo "arm_64bit=1" >> ${BOOTFS}/config.txt
+        echo "arm_64bit=1" >> ${OUTPUT_DIR}/config.txt
     fi
 
     # show config
-    cat ${BOOTFS}/config.txt
+    cat ${OUTPUT_DIR}/config.txt
 }
 
 # use debian standard kernel + rpi firmware package
 kernel_standard(){
     # add firmware files
-    cp -RT ${BUILDFS}/usr/lib/raspi-firmware ${BOOTFS}
-    chmod -R 0777 ${BOOTFS}
+    cp -RT ${TARGET_FS}/usr/lib/raspi-firmware ${OUTPUT_DIR}
+    chmod -R 0777 ${OUTPUT_DIR}
 
     # add kernel config to config.txt
-    echo "# -- hypersolid automated boot configuration" >> ${BOOTFS}/config.txt
-    echo "kernel=kernel.img" >> ${BOOTFS}/config.txt
-    echo "arm_64bit=1" >> ${BOOTFS}/config.txt
+    echo "# -- hypersolid automated boot configuration" >> ${OUTPUT_DIR}/config.txt
+    echo "kernel=kernel.img" >> ${OUTPUT_DIR}/config.txt
+    echo "arm_64bit=1" >> ${OUTPUT_DIR}/config.txt
 }
+
+# copy additional config to output dir
+cp -RT ${TARGET_FS}/etc/rpi-boot-config ${OUTPUT_DIR}
 
 # choose kernel flavor
 case ${RASPBERRY_KERNEL_TYPE} in
